88eabd77404afd03fa968a9101b533b911809f72
George <[Email>
2023-10-09 20:55:52 +0100

    Adding clean script

--- python/plotting.py
+++ python/plotting.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import matplotlib.patches as mpatches
+from matplotlib.lines import Line2D
 import mplhep as hep
-import copy
 
 hep.style.use("CMS")
    
@@ -20,7 +22,6 @@ def plot_histogram_with_ratio(
       use_stat_err=False,
       ):
     fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios': [3, 1]})
-
     bin_centers = bin_edges[:-1] + np.diff(bin_edges) / 2  # Compute bin centers
 
     hist_values1 = hist_values1.astype(np.float64)
@@ -73,9 +74,17 @@ def plot_histogram_with_ratio(
         transform=ax1.transAxes)
 
     # Compute the ratio of the histograms
+    zero_indices = np.where(hist_values1 == 0)
+    for i in zero_indices: hist_values1[i] = 1.0
+
     ratio = np.divide(hist_values2,hist_values1)
     ratio_errors_1 = np.divide(errors_1,hist_values1)
     ratio_errors_2 = np.divide(errors_2,hist_values1)
+
+    for i in zero_indices:
+       ratio[i] = 0.0
+       ratio_errors_1[i] = 0.0
+       ratio_errors_2[i] = 0.0
 
     # Plot the ratio on the bottom pad
     ax2.errorbar(bin_centers, ratio, fmt='o', yerr=ratio_errors_2, label=name_2, color="black")
@@ -94,13 +103,28 @@ def plot_histogram_with_ratio(
     plt.savefig(name+".pdf")
     plt.close()
 
-def plot_likelihood(x, y, crossings, name="lkld", xlabel="", true_value=None, cap_at_3=True):
+def plot_likelihood(x, y, crossings, name="lkld", xlabel="", true_value=None, cap_at=9):
+
+  if cap_at != None:
+    x_plot = []
+    y_plot = []
+    for ind, i in enumerate(y):
+      if i < cap_at:
+        x_plot.append(x[ind])
+        y_plot.append(i)
+    x = x_plot
+    y = y_plot
+    y_max = cap_at
+  else:
+    y_max = max(y)
+
   fig, ax = plt.subplots()
   hep.cms.text("Work in progress",ax=ax)
   plt.plot(x, y)
 
   if true_value != None:
-    plt.plot([true_value,true_value], [0,9], linestyle='--', color='black')
+    plt.plot([true_value,true_value], [0,y_max], linestyle='--', color='black')
+
     ax.text(1.0, 1.0, "True Value: {}".format(true_value),
         verticalalignment='bottom', horizontalalignment='right',
         transform=ax.transAxes)
@@ -117,9 +141,50 @@ def plot_likelihood(x, y, crossings, name="lkld", xlabel="", true_value=None, ca
   plt.plot([x[0],x[-1]], [4,4], linestyle='--', color='gray')
   
   plt.xlim(x[0],x[-1])
-  if cap_at_3:
-    plt.ylim(0,9)
+  plt.ylim(0,y_max)
   plt.xlabel(xlabel)
   plt.ylabel(r'$-2\Delta \ln L$')
   print("Created "+name+".pdf")
   plt.savefig("{}.pdf".format(name))
+  plt.close()
+
+
+def plot_2d_likelihood(x, y, z, name="lkld_2d", xlabel="", ylabel="", best_fit=None, true_value=None, title_right=""):
+
+  fig, ax = plt.subplots()
+  hep.cms.text("Work in progress",ax=ax)
+
+  plt.scatter([best_fit[0]], [best_fit[1]], marker='+', s=400, color='Black', label='Best Fit')
+  plt.scatter([true_value[0]], [true_value[1]], marker='x', s=400, color='Black', label='True Value')
+
+  c1 = (204/255,204/255,255/255)
+  c2 = (153/255,153/255,204/255)
+  cmap1 = mcolors.LinearSegmentedColormap.from_list("custom", [c1, "white"], N=256)
+  cmap2 = mcolors.LinearSegmentedColormap.from_list("custom", [c2, "white"], N=256)
+  plt.contourf(x, y, z, levels=[0.0, 5.99], cmap=cmap1)
+  plt.contourf(x, y, z, levels=[0.0, 2.28], cmap=cmap2)
+  plt.contour(x, y, z, levels=[0.0, 5.99], colors=["black"])
+  plt.contour(x, y, z, levels=[0.0, 2.28], colors=["black"])
+
+  legend_handles = [
+      Line2D([0], [0], color=c1, lw=15, label='95% CL'),
+      Line2D([0], [0], color=c2, lw=15, label='68% CL')
+  ]
+  
+  scatter1 = plt.scatter([best_fit[0]], [best_fit[1]], marker='+', s=400, color='Black', label='Best Fit')
+  scatter2 = plt.scatter([true_value[0]], [true_value[1]], marker='x', s=400, color='Black', label='True Value')
+
+  legend_handles.append(scatter1)
+  legend_handles.append(scatter2)
+
+  plt.legend(handles=legend_handles)
+
+  ax.text(1.0, 1.0, title_right,
+      verticalalignment='bottom', horizontalalignment='right',
+      transform=ax.transAxes)
+
+  plt.xlabel(xlabel)
+  plt.ylabel(ylabel)
+  print("Created "+name+".pdf")
+  plt.savefig("{}.pdf".format(name))
+  plt.close()
